f9145084794e3a0d0fb75b7b9c6b9bb37cc39195
Casey Gerena <caseygerena@gmail.com>
2021-01-27 23:58:17 -0500

    added new logo and dropdown menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import './style.scss';
-import Navigation from './navigation';
+import Nav from './nav';
 import McsVideo from '../assets/mcs-logo-animation.mp4';
 
 const Header = ({ siteTitle }) => (
-	<section className="hero gradientBg is-fullheight-with-navbar">
-		<Navigation />
-		<div className="hero-body">
-			<div className="container center">
-				<article className="media">
-					<div className="media-content">
-						<div className="content">
-							<video width="800" height="600" playsInline autoPlay muted loop>
-								<source src={McsVideo} type="video/mp4" />
-								Your browser does not support the video tag.
-							</video> 
-							<h1 className="is-uppercase is-size-1 has-text-white">
-								Mocu Gaming Squad
-							</h1>
-							<p className="subtitle has-text-white is-size-3">
-								A multi-game team
-							</p>
-						</div>
-					</div>
-				</article>
-			</div>
-		</div>
-	</section>
+  <section className="hero gradientBg is-fullheight-with-navbar">
+    <Nav />
+    <div className="hero-body">
+      <div className="container center">
+        <article className="media">
+          <div className="media-content">
+            <div className="content">
+              <video
+                width="800"
+                height="600"
+                playsInline
+                autoPlay
+                muted
+                loop
+              >
+                <source src={McsVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+              <h1 className="is-uppercase is-size-1 has-text-white">
+                Mocu Gaming Squad
+              </h1>
+              <p className="subtitle has-text-white is-size-3">
+                A multi-game team
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
 );
 
 export default Header;
---
+++ src/components/nav.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './style.scss';
+import mcsNavLogo from '../images/mcs-nav-logo.png';
+import { Link } from 'gatsby';
+
+const Nav = () => (
+  <nav
+    className="navbar is-dark"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img src={mcsNavLogo} width="62" />
+        </a>
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+
+          <a className="navbar-item">News</a>
+
+          <a className="navbar-item">About</a>
+
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">Team</a>
+
+            <div className="navbar-dropdown">
+              <a className="navbar-item">Apex Legends</a>
+              <a className="navbar-item">Diabotical</a>
+              <a className="navbar-item">World of Warcraft</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Tryout for our squad</a>
+            </div>
+          </div>
+
+          <a className="navbar-item">Store</a>
+          <a className="navbar-item">Contact</a>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-light">
+                <strong>Scrim us</strong>
+              </a>
+              <a className="button is-dark">Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Nav;
